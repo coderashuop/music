4bad449b042d0d1352d09959a6445038633fc325
coderashuop <ashumd768@gmail.com>
2025-06-15 13:49:10 +0545

    Update start.py
    Signed-off-by: coderashuop <ashumd768@gmail.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -29,9 +29,6 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_CHANNEL),
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
     
     ]
     
